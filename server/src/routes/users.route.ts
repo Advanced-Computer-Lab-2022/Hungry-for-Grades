cf289418eb80abf656eb39e7296470d8e1c2c02c
Omar Sherif Ali <omar.sherif9992@gmail.com>
2022-10-21 02:21:04 +0200

    fix: prettierrc

--- server/src/routes/users.route.ts
+++ server/src/routes/users.route.ts
@@ -1,49 +1,24 @@
-import { Router } from 'express';
-import UsersController from '@controllers/users.controller';
 import { CreateUserDto } from '@/dtos/users.dto';
+import UsersController from '@controllers/users.controller';
 import { Routes } from '@interfaces/routes.interface';
 import validationMiddleware from '@middlewares/validation.middleware';
+import { Router } from 'express';
 
 class UsersRoute implements Routes {
   public path = '/users';
   public router = Router();
-  public usersController =
-    new UsersController();
+  public usersController = new UsersController();
 
   constructor() {
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
-    this.router.get(
-      '',
-      this.usersController.getUsers,
-    );
-    this.router.get(
-      '/:id',
-      this.usersController.getUserById,
-    );
-    this.router.post(
-      '',
-      validationMiddleware(
-        CreateUserDto,
-        'body',
-      ),
-      this.usersController.createUser,
-    );
-    this.router.put(
-      '/:id',
-      validationMiddleware(
-        CreateUserDto,
-        'body',
-        true,
-      ),
-      this.usersController.updateUser,
-    );
-    this.router.delete(
-      '/:id',
-      this.usersController.deleteUser,
-    );
+    this.router.get('', this.usersController.getUsers);
+    this.router.get('/:id', this.usersController.getUserById);
+    this.router.post('', validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
+    this.router.put('/:id', validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
+    this.router.delete('/:id', this.usersController.deleteUser);
   }
 }
 
