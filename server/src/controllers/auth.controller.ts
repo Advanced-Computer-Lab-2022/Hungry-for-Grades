f63758dd04d3e7c0cb0ce948e4e216b58cc56238
Omar Sherif Ali <omar.sherif9992@gmail.com>
2022-10-22 01:57:46 +0200

    fix(src): refactored directories
    fix #16

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,8 +1,8 @@
-import { NextFunction, Request, Response } from 'express';
+import AuthService from '@/DAO/auth.dao';
 import { CreateUserDto } from '@/dtos/users.dto';
-import { RequestWithUser } from '@interfaces/auth.interface';
 import { User } from '@/interfaces/user.interface';
-import AuthService from '@services/auth.service';
+import { RequestWithUser } from '@interfaces/auth.interface';
+import { NextFunction, Request, Response } from 'express';
 
 class AuthController {
   public authService = new AuthService();
