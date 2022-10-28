f0b77a34c8ddbf2187c3fb2ff5025f53a5a6b129
HusseinYasser <husseinyasseer388@gmail.com>
2022-10-28 04:07:52 -0700

    feat: a

--- client/src/pages/landing/Landing.tsx
+++ client/src/pages/landing/Landing.tsx
@@ -4,11 +4,8 @@ import MainPageSection1 from '@components/HomeSection/MainPageSection1';
 
 import InstructorBillboard from '@/components/instructorBillboard/InstructorBillboard';
 
-<<<<<<< HEAD
 import InstructorMainSection from '@/components/Instructor/InstructorMainSection';
 
-=======
->>>>>>> development
 function Landing() {
   return (
     <>
