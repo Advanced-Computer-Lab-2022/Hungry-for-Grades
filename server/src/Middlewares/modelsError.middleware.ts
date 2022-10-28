67a5b47ab8be4348bebbd937341fe149120a8492
AWahba <ahmedheshamwahba@hotmail.com>
2022-10-28 19:40:27 +0200

    fix(middleware): fixed mispelling when importing
    #23

--- server/src/Middlewares/modelsError.middleware.ts
+++ server/src/Middlewares/modelsError.middleware.ts
@@ -1,5 +1,5 @@
-import { HttpResponse } from '@/Utils/HttpResponsee';
-import { logger } from '@/Utils/loggerr';
+import { HttpResponse } from '@/Utils/HttpResponse';
+import { logger } from '@/Utils/logger';
 import { NextFunction, Request, Response } from 'express';
 import { Error } from 'mongoose';
 
