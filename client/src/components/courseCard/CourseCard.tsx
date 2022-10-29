856477f0a9b50f578cfd1bbead628bd540dded6a
Omar Sherif Ali <omar.sherif9992@gmail.com>
2022-10-29 18:33:54 +0200

    feat: course overlay

--- client/src/components/courseCard/CourseCard.tsx
+++ client/src/components/courseCard/CourseCard.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable import/order */
 import { formatCurrency } from '@/utils/currency';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import { Link } from 'react-router-dom';
 import StarRatings from 'react-star-ratings';
 import { CourseCardProps } from '../../pages/landing/types';
 import styles from './courseCard.module.scss';
+import CourseCardOverlay from './CourseInstructorCardOverlay';
 
 function Price(props: CourseCardProps) {
   if (props.originalPrice) {
@@ -25,27 +27,32 @@ function Price(props: CourseCardProps) {
 
 function CourseCard(props: CourseCardProps) {
   return (
+
     <Link to={`/course/${props.id}`}>
-      <article className={` card rounded bg-light shadow `}>
-        <div className={`p-5 mx-auto`}>
+			<OverlayTrigger
+      overlay={<div><CourseCardOverlay/></div>}
+      placement="right"
+    >
+      <article
+        className={`${
+          styles.course__card ?? ''
+        } card card-cascade rounded bg-light shadow `}
+      >
+        <div className={`${styles.course__img__container ?? ''}`}>
           <img
             alt={props.title}
-            className=' card-img-top img-fluid'
-            height='80%'
+            className={`card-img-top img-fluid ${styles.course__img ?? ''}`}
             src={props.image}
-            width='80%'
           />
         </div>
-        <div>
-          <div>
-            <h4
-              className={`${styles['course-title'] ?? ''} ${
-                styles['fnt-md'] ?? ''
-              } card-title text-dark text-left`}
-            >
-              {props.title}
-            </h4>
-          </div>
+        <div className='card-body'>
+          <h4
+            className={`${styles.course__title ?? ''} ${
+              styles['fnt-md'] ?? ''
+            } card-title text-dark text-left `}
+          >
+            {props.title}
+          </h4>
           <div className={`${styles['fnt-xs'] ?? ''}`}>
             {props.instructors.map(instructor => instructor.name).join(', ')}
           </div>
@@ -62,14 +69,15 @@ function CourseCard(props: CourseCardProps) {
                 numberOfStars={5}
                 rating={props.rating}
                 starDimension='20px'
-                starRatedColor='rgb(229, 152, 25)'
-                starSpacing='0px'
+								starRatedColor="#FFD700"
+								starSpacing="0px"
               />
             </span>
           </div>
           <Price {...props} />
         </div>
       </article>
+			</OverlayTrigger>
     </Link>
   );
 }
