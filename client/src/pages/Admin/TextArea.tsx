d941f428dc319012f6c3e6ac43a1d88a253bbcb8
Omar Sherif Ali <omar.sherif9992@gmail.com>
2022-10-29 22:20:40 +0200

    feat: fix

--- client/src/pages/Admin/TextArea.tsx
+++ client/src/pages/Admin/TextArea.tsx
@@ -2,22 +2,28 @@ import { ErrorMessage, useField } from 'formik';
 
 import styles from './TextArea.module.scss';
 
-
-export default function TextArea(props: {name:string, placeholder:string, type:string}) {
-
-    const [field, meta] = useField(props);
+export default function TextArea(props: {
+  name: string;
+  placeholder: string;
+  type: string;
+}) {
+  const [field, meta] = useField(props);
 
   return (
     <>
-       <input
+      <input
         className={`
-        ${styles.textArea,  meta.error && meta.touched && 'is-invalid' || ''}
+        ${(styles.textArea, (meta.error && meta.touched && 'is-invalid') || '')}
         `}
         {...field}
         {...props}
-        autoComplete="off"
+        autoComplete='off'
       />
-      <ErrorMessage className={styles.error} component="div" name={field.name} />
+      <ErrorMessage
+        className={styles.error}
+        component='div'
+        name={field.name}
+      />
     </>
-  )
+  );
 }
