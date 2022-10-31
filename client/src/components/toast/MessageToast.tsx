186439eb29e36c3ab5ad87759823ca096c6cf454
omar-elmeteny <omar.elmeteny@gmail.com>
2022-10-31 21:56:03 +0200

    fix(toast): removed import for toastify css because it's already in main.scss

--- client/src/components/toast/MessageToast.tsx
+++ client/src/components/toast/MessageToast.tsx
@@ -1,6 +1,6 @@
 import { ToastContainer } from 'react-toastify';
 
-import 'react-toastify/dist/ReactToastify.css';
+// import 'react-toastify/dist/ReactToastify.css';
 
 import { createPortal } from 'react-dom';
 
