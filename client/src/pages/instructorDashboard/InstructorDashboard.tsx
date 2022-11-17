f1bf1da620c4959d9a5e21ace0dbf09d82b9dfd6
HusseinYasser <husseinyasseer388@gmail.com>
2022-11-17 14:51:55 -0800

    feat(updating lint): updating lint errors

--- client/src/pages/instructorDashboard/InstructorDashboard.tsx
+++ client/src/pages/instructorDashboard/InstructorDashboard.tsx
@@ -1,18 +1,29 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import InstructorCoursesSection from './InstructorCoursesSection';
 import useSearchQuery from './useSearchQuery';
 import { useSeletedFilters } from './useSelectedFilters';
-import SearchSection from './searchSection/SearchSection';
+
 function InstructorDashboard() {
   const [selectedFilters, setSelectedFilters] = useSeletedFilters();
   const { isLoading, isError, data, error } = useSearchQuery(selectedFilters);
   if (isError) return <div>{error?.message}</div>;
   return (
     <>
-      <SearchSection
-        selectedFilters={selectedFilters}
-        setSelectedFilters={setSelectedFilters}
-      />
+      <div className='container px-0 my-3'>
+        <input
+          aria-describedby='search-addon'
+          aria-label='Search'
+          className='form-control rounded input'
+          name={'searchTerm'}
+          placeholder='Search by the course name ...'
+          type='search'
+          value={selectedFilters.searchTerm}
+          onChange={e => {
+            setSelectedFilters(prev => {
+              return { ...prev, [e.target.name]: e.target.value };
+            });
+          }}
+        />
+      </div>
       {!isLoading && <InstructorCoursesSection {...data?.data} />}
     </>
   );
