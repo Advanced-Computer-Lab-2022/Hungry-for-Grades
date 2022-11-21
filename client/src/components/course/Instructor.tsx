876475a4bd5bf9cbb2cd3092ae18d8e584b9cadd
omar-elmeteny <omar.elmeteny@gmail.com>
2022-11-21 12:40:36 +0200

    fix(instructor): fixed instructor type
    fixed instructor type to be the same as the instructor type returned from the server

--- client/src/components/course/Instructor.tsx
+++ client/src/components/course/Instructor.tsx
@@ -1,40 +1,25 @@
 import { Link } from 'react-router-dom';
 
 import { Instructor } from '@/interfaces/course.interface';
-import { IUser } from '@/interfaces/user.interface';
 
 type InstructorsProps = {
-  instructor: Instructor | Instructor[];
+  instructors: Instructor[];
   linkClassName?: string;
 };
 
 function Instructors(props: InstructorsProps) {
-  let users: IUser[];
-  if (Array.isArray(props.instructor)) {
-    users = props.instructor.reduce(
-      (prev, curr) =>
-        Array.isArray(curr._user)
-          ? [...prev, ...curr._user]
-          : [...prev, curr._user],
-      [] as IUser[]
-    );
-  } else {
-    users = Array.isArray(props.instructor._user)
-      ? props.instructor._user
-      : [props.instructor._user];
-  }
   return (
     <span>
-      {users.map((user, index) => (
-        <span key={user._id}>
+      {props.instructors.map((i, index) => (
+        <span key={i._id}>
           <Link
             className={props.linkClassName}
-            title={user.name}
-            to={`/instructor/${user._id}`}
+            title={i.name}
+            to={`/instructor/${i._id}`}
           >
-            {user.name}
+            {i.name}
           </Link>
-          {index < users.length - 1 ? `, ` : ``}
+          {index < props.instructors.length - 1 ? `, ` : ``}
         </span>
       ))}
     </span>
