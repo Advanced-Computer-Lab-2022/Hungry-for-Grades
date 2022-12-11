3808d42056d9947b375fc8b347edbc8338f6575d
Omar Sherif Ali <omar.sherif9992@gmail.com>
2022-12-11 19:50:32 +0200

    feat: main section

--- client/src/pages/guest/landing/mainSection/MainSection.tsx
+++ client/src/pages/guest/landing/mainSection/MainSection.tsx
@@ -4,126 +4,108 @@ import { Link } from 'react-router-dom';
 import './mainSection.scss';
 export default function MainSection() {
   return (
-    <>
-      <section className='slider '>
-        <div
-          className='carousel slide'
-          data-bs-ride='carousel'
-          id='carouselExampleCaptions'
-        >
-          <div className='carousel-inner '>
-            <div className='carousel-item active vh-100'>
-              <img
-                alt='slideshow-1'
-                className='d-block  w-100 img1 img-fluid'
-                src='https://learning.linkedin.com/content/dam/me/business/en-us/amp/learning-solutions/images/lls-homepage-2021/bg/01-dsk-b02-v01.jpg/jcr:content/renditions/01-dsk-b02-v01-2x.jpg'
-                style={{
-                  height: '50%',
-                  width: '100%'
-                }}
-              />
-              <div className='carousel-caption  d-sm-none d-lg-block  position-absolute'>
-                <h3 className='text-black'>Focused on strategy</h3>
-                <h4 className='text-primary'>To Reach Your Goal Faster</h4>
-                <div className='d-flex justify-content-start mt-3'>
-                  <Link className='btn btn-primary btn-lg' to={'courses'}>
-                    courses
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className='carousel-item vh-100  '>
-              <img
-                alt='slideshow-2'
-                className='d-block w-100 img2'
-                src='https://s.udemycdn.com/teaching/billboard-mobile-v3.jpg'
-                style={{
-                  height: '50%',
-                  width: '100%'
-                }}
-              />
-              <div className='carousel-caption d-md-block position-absolute text-black'>
-                <h3> Take your career to the next level</h3>
-                <h4 className='text-primary'>
-                  Access to a collection of top-rated courses in tech, business,
-                  and more.
-                </h4>
-              </div>
-            </div>
-            <div className='carousel-item vh-100'>
-              <img
-                alt='slideshow-3'
-                className='d-block w-100 img-fluid img3'
-                src='https://s.udemycdn.com/consumer-subscription/pillars/pillars-1-desktop-v2.jpg'
-                style={{
-                  height: '50%',
-                  width: '100%'
-                }}
-              />
-
-              <div className='carousel-caption  d-md-block position-absolute '>
-                <h3 className='text-black'>
-                  Cutting-edge skills to keep you sharp
-                </h3>
-                <h4 className='text-primary'>
-                  Learn confidently with up-to-date courses
-                </h4>
+    <section className='slider'>
+      <div
+        className='carousel slide'
+        data-bs-ride='carousel'
+        id='carouselExampleCaptions'
+      >
+        <div className='carousel-inner '>
+          <div className='carousel-item active vh-100'>
+            <img
+              alt='slideshow-1'
+              className='d-block vh-40  w-100 img1 img-fluid'
+              src='https://learning.linkedin.com/content/dam/me/business/en-us/amp/learning-solutions/images/lls-homepage-2021/bg/01-dsk-b02-v01.jpg/jcr:content/renditions/01-dsk-b02-v01-2x.jpg'
+              style={{
+                height: '50%',
+                width: '100%'
+              }}
+            />
+            <div className='carousel-caption  d-sm-none d-lg-block  position-absolute'>
+              <h3 className='text-black'>Focused on strategy</h3>
+              <h4 className='text-primary'>To Reach Your Goal Faster</h4>
+              <div className='d-flex justify-content-start mt-3'>
+                <Link className='btn btn-primary btn-lg' to={'courses'}>
+                  courses
+                </Link>
               </div>
             </div>
           </div>
-          <div>
-            <button
-              className='carousel-control-prev'
-              data-bs-slide='prev'
-              data-bs-target='#carouselExampleCaptions'
+          <div className='carousel-item vh-100  '>
+            <img
+              alt='slideshow-2'
+              className='d-block w-100 img2'
+              src='https://s.udemycdn.com/teaching/billboard-mobile-v3.jpg'
               style={{
-                zIndex: 999,
-                top: '35%'
+                height: '50%',
+                width: '100%'
               }}
-              type='button'
-            >
-              <BiChevronLeft className='icon' />
-              <span className='visually-hidden'>Previous</span>
-              <div className='lay' />
-            </button>
-            <button
-              className='carousel-control-next'
-              data-bs-slide='next'
-              data-bs-target='#carouselExampleCaptions'
+            />
+            <div className='carousel-caption d-md-block position-absolute text-black'>
+              <h3> Take your career to the next level</h3>
+              <h4 className='text-primary'>
+                Access to a collection of top-rated courses in tech, business,
+                and more.
+              </h4>
+            </div>
+          </div>
+          <div className='carousel-item vh-100'>
+            <img
+              alt='slideshow-3'
+              className='d-block w-100 img-fluid img3'
+              src='https://s.udemycdn.com/consumer-subscription/pillars/pillars-1-desktop-v2.jpg'
               style={{
-                zIndex: 999,
-                top: '40%'
+                height: '50%',
+                width: '100%'
               }}
-              type='button'
-            >
-              <BiChevronRight
-                className='icon'
-                style={{
-                  top: '-30%'
-                }}
-              />
-              <span className='visually-hidden'>Next</span>
-              <div className='lay' />
-            </button>
+            />
+
+            <div className='carousel-caption  d-md-block position-absolute '>
+              <h3 className='text-black'>
+                Cutting-edge skills to keep you sharp
+              </h3>
+              <h4 className='text-primary'>
+                Learn confidently with up-to-date courses
+              </h4>
+            </div>
           </div>
         </div>
-      </section>
-      {/*      <div
-        className='fluid-container'
-        style={{
-          width: '100%',
-          height: '32rem',
-          background: '#EFF0F6'
-        }}
-      >
-        <img
-          alt='CanCham'
-          className={`${styles.img_item ?? ''} img-fluid `}
-          loading='lazy'
-          src='https://learning.linkedin.com/content/dam/me/business/en-us/amp/learning-solutions/images/lls-homepage-2021/bg/01-dsk-b02-v01.jpg/jcr:content/renditions/01-dsk-b02-v01-2x.jpg'
-        />
-
-      </div> */}
-    </>
+        <div>
+          <button
+            className='carousel-control-prev'
+            data-bs-slide='prev'
+            data-bs-target='#carouselExampleCaptions'
+            style={{
+              zIndex: 999,
+              top: '35%'
+            }}
+            type='button'
+          >
+            <BiChevronLeft className='icon' />
+            <span className='visually-hidden'>Previous</span>
+            <div className='lay' />
+          </button>
+          <button
+            className='carousel-control-next'
+            data-bs-slide='next'
+            data-bs-target='#carouselExampleCaptions'
+            style={{
+              zIndex: 999,
+              top: '40%'
+            }}
+            type='button'
+          >
+            <BiChevronRight
+              className='icon'
+              style={{
+                top: '-30%'
+              }}
+            />
+            <span className='visually-hidden'>Next</span>
+            <div className='lay' />
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
--- client/src/pages/guest/searchCourses/searchSection/filtersInput/useCategoryQuery.tsx
+++ client/src/pages/guest/searchCourses/searchSection/filtersInput/useCategoryQuery.tsx
@@ -1,6 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
 
-
 import { CategoryRoute } from '@services/axios/dataServices/CategoryDataService';
 
 import { getRequest } from '@services/axios/http-verbs';
@@ -9,8 +8,11 @@ import { HttpResponse } from '@/interfaces/response.interface';
 type CategoryType = { label: string; subcategory: { label: string }[] }[];
 
 async function categoryRequest() {
-  return (await getRequest<HttpResponse<CategoryType>>(CategoryRoute.GET.getCategories))
-    ?.data;
+  return (
+    await getRequest<HttpResponse<CategoryType>>(
+      CategoryRoute.GET.getCategories
+    )
+  )?.data;
 }
 
 function useCategoryQuery() {
