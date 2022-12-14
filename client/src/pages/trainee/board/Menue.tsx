7b50cc46a743177fe55ab44b17dfeb5016e62a15
Omar Sherif Ali <omar.sherif9992@gmail.com>
2022-12-14 13:35:16 +0200

    feat: board

--- client/src/pages/trainee/board/Menue.tsx
+++ client/src/pages/trainee/board/Menue.tsx
@@ -1,37 +1,68 @@
 import './board.scss';
 
-function Menu({ setLineColor, setLineWidth, setLineOpacity }) {
+function Menu({
+  lineColor,
+  setLineColor,
+  setLineWidth,
+  setLineOpacity,
+  canvasRef,
+  lineWidth,
+  lineOpacity
+}: {
+  setLineColor: (color: string) => void;
+  setLineWidth: (width: number) => void;
+  setLineOpacity: (opacity: number) => void;
+  canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
+  lineColor: string;
+  lineWidth: number;
+  lineOpacity: number;
+}) {
   return (
     <div className='Menu'>
       <label htmlFor='color'>Brush Color </label>
       <input
-			name='color'
+        name='color'
         type='color'
+        value={lineColor}
         onChange={e => {
           setLineColor(e.target.value);
         }}
       />
-      <label  htmlFor='brush'>Brush Width </label>
+      <label htmlFor='brush'>Brush Width </label>
       <input
-						max='20'
-
+        max='20'
         min='3'
         name='brush'
         type='range'
+        value={lineWidth}
         onChange={e => {
-          setLineWidth(e.target.value);
+          setLineWidth(parseInt(e.target.value));
         }}
       />
       <label htmlFor='opacity'>Brush Opacity</label>
       <input
-			max='100'
+        max='100'
         min='1'
         name='opacity'
         type='range'
+        value={lineOpacity*100}
         onChange={e => {
-          setLineOpacity(e.target.value / 100);
+          setLineOpacity(() => parseInt(e.target.value) / 100);
         }}
       />
+
+      <button
+        className='btn btn-primary'
+        type='button'
+        onClick={function onSave() {
+          const link = document.createElement('a');
+          link.download = `${Date.now()}.jpg`;
+          link.href = canvasRef?.current?.toDataURL();
+          link.click();
+        }}
+      >
+        Save As Image
+      </button>
     </div>
   );
 }
--- client/src/pages/trainee/board/TraineeBoard.tsx
+++ client/src/pages/trainee/board/TraineeBoard.tsx
@@ -8,55 +8,88 @@ function App() {
   const ctxRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(false);
   const [lineWidth, setLineWidth] = useState(5);
-  const [lineColor, setLineColor] = useState('black');
+  const [lineColor, setLineColor] = useState('red');
   const [lineOpacity, setLineOpacity] = useState(0.1);
+  const [prevMouse, setPrevMouse] = useState({ prevMouseY: 0, prevMouseX: 0 });
 
   // Initialization when the component
   // mounts for the first time
   useEffect(() => {
     const canvas = canvasRef.current;
+    console.log(canvas);
     const ctx = canvas?.getContext('2d');
+
     ctx.lineCap = 'round';
     ctx.lineJoin = 'round';
     ctx.globalAlpha = lineOpacity;
     ctx.strokeStyle = lineColor;
+    ctx.fillStyle = lineColor;
+
     ctx.lineWidth = lineWidth;
     ctxRef.current = ctx;
-  }, [lineColor, lineOpacity, lineWidth]);
+
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+  }, []);
 
   // Function for starting the drawing
-  const startDrawing = e => {
+  function startDrawing(e) {
+    /*     setPrevMouse(() => ({
+      prevMouseX: e.offsetX,
+      prevMouseY: e.offsetY
+    })); */
+    if (!ctxRef && !ctxRef.current) {
+      alert('sss');
+      return;
+    }
+    const ctx = {
+      globalAlpha: lineOpacity,
+      strokeStyle: lineColor,
+      fillStyle: lineColor,
+      lineWidth: lineWidth
+    };
+
+    ctxRef.current.fillStyle = lineColor;
+    ctxRef.current.strokeStyle = lineColor;
+    ctxRef.current.globalAlpha = lineOpacity;
+    ctxRef.current.lineWidth = lineWidth;
+
     ctxRef?.current?.beginPath();
     ctxRef?.current?.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
     setIsDrawing(true);
-  };
+  }
 
   // Function for ending the drawing
-  const endDrawing = () => {
+  function endDrawing() {
     ctxRef.current.closePath();
     setIsDrawing(false);
-  };
+  }
 
-  const draw = e => {
+  function draw(e) {
     if (!isDrawing) {
       return;
     }
     ctxRef.current.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
 
     ctxRef.current.stroke();
-  };
+  }
 
   return (
     <div className='container paint'>
       <div className='App'>
         <div className='draw-area'>
           <Menu
+            canvasRef={canvasRef}
+            lineColor={lineColor}
+            lineOpacity={lineOpacity}
+            lineWidth={lineWidth}
             setLineColor={setLineColor}
             setLineOpacity={setLineOpacity}
             setLineWidth={setLineWidth}
           />
           <canvas
             ref={canvasRef}
+            className='canvas'
             height={`720px`}
             width={`1280px`}
             onMouseDown={startDrawing}
