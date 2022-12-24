457247a57a668d2ca8d0d2c957ff36e7711bf2b0
Omar Sherif Ali <omar.sherif9992@gmail.com>
2022-12-24 13:17:41 +0200

    feat: store

--- client/src/services/cookie/Cookie.ts
+++ client/src/services/cookie/Cookie.ts
@@ -1,81 +1,81 @@
 class Cookie {
-	// to check if cookie exists
-	isFound(key: string) {
-		return document.cookie.indexOf(key) !== -1;
-	}
+  // to check if cookie exists
+  isFound(key: string) {
+    return document.cookie.indexOf(key) !== -1;
+  }
 
-	// get cookie value by key name
-	get(key: string) {
-		if (this.isFound(key)) {
-			return document.cookie
-				.split(';')
-				.filter(item => {
-					return item.indexOf(key) !== -1;
-				})
-				.join(';');
-		} else {
-			return null;
-		}
-	}
+  // get cookie value by key name
+  get(key: string) {
+    if (this.isFound(key)) {
+      return document.cookie
+        .split(';')
+        .filter(item => {
+          return item.indexOf(key) !== -1;
+        })
+        .join(';');
+    } else {
+      return null;
+    }
+  }
 
-	// to set cookie value
-	set(key: string, value: string, expiresMonth: number) {
-		if (!this.isFound(key)) {
-			const date = new Date();
-			date.setMonth(date.getMonth() + expiresMonth);
-			document.cookie += `${key}=${value};expires=${date.toUTCString()}`;
-		} else {
-			this.update(key, value, expiresMonth);
-		}
-	}
+  // to set cookie value
+  set(key: string, value: string, expiresMonth: number) {
+    if (!this.isFound(key)) {
+      const date = new Date();
+      date.setMonth(date.getMonth() + expiresMonth);
+      document.cookie += `${key}=${value};expires=${date.toUTCString()}`;
+    } else {
+      this.update(key, value, expiresMonth);
+    }
+  }
 
-	// update cookie value
-	update(key: string, value: string, expiresMonth: number) {
-		if (this.isFound(key)) {
-			this.remove(key);
-			this.set(key, value, expiresMonth);
-		} else {
-			throw new Error('Cookie not found to be updated');
-		}
-	}
+  // update cookie value
+  update(key: string, value: string, expiresMonth: number) {
+    if (this.isFound(key)) {
+      this.remove(key);
+      this.set(key, value, expiresMonth);
+    } else {
+      throw new Error('Cookie not found to be updated');
+    }
+  }
 
-	// to remove cookie value
-	remove(key: string) {
-		if (this.isFound(key)) {
-			return document.cookie
-				.split(';')
-				.filter(item => {
-					return item !== key;
-				})
-				.join(';');
-		} else {
-			throw new Error('Cookie not found to be removed');
-		}
-	}
+  // to remove cookie value
+  remove(key: string) {
+    if (this.isFound(key)) {
+      return document.cookie
+        .split(';')
+        .filter(item => {
+          return item !== key;
+        })
+        .join(';');
+    } else {
+      throw new Error('Cookie not found to be removed');
+    }
+  }
 
-	// to get cookie keys
-	getKeys() {
-		return document.cookie.split(';').map(item => {
-			return item.split('=')[0];
-		});
-	}
+  // to get cookie keys
+  getKeys() {
+    return document.cookie.split(';').map(item => {
+      return item.split('=')[0];
+    });
+  }
 
-	// to get cookie values
-	getValues() {
-		return document.cookie.split(';').map(item => {
-			return item.split('=')[1];
-		});
-	}
+  // to get cookie values
+  getValues() {
+    return document.cookie.split(';').map(item => {
+      return item.split('=')[1];
+    });
+  }
 
-	// to get all cookies
-	getAll() {
-		return document.cookie.split(';');
-	}
+  // to get all cookies
+  getAll() {
+    return document.cookie.split(';');
+  }
 
-	// to clear all cookies
-	clear() {
-		document.cookie = '';
-	}
+  // to clear all cookies
+  clear() {
+    document.cookie = '';
+  }
 }
 
 export default new Cookie();
--- client/src/store/userStore.ts
+++ client/src/store/userStore.ts
@@ -9,58 +9,57 @@ import { EnrolledCourse, ITrainee } from '@/interfaces/course.interface';
 import { type IUser } from '@interfaces/user.interface';
 
 export interface IUserStore {
-	user: IUser | null;
-	setUser: (user: IUser) => void;
-	getUser: () => IUser | null;
-	logOut: () => void;
-	isAuthenticated: boolean | null;
-	setIsAuthenticated: (isAuthenticated: boolean) => void;
-	isEnrolled: (id: string) => boolean;
-	setBalance: (balance: number) => void
+  user: IUser | null;
+  setUser: (user: IUser) => void;
+  getUser: () => IUser | null;
+  logOut: () => void;
+  isAuthenticated: boolean | null;
+  setIsAuthenticated: (isAuthenticated: boolean) => void;
+  isEnrolled: (id: string) => boolean;
+  setBalance: (balance: number) => void;
 }
 
 export const useUserStore = create<IUserStore, [['zustand/devtools', never]]>(
-	devtools((set, get) => ({
-		user: SessionStorage.get<IUser>('user') as IUser | null,
-		isAuthenticated: SessionStorage.get<IUser>('user') ? true : null,
-		setUser: (user: IUser) => {
-			SessionStorage.set<IUser>('user', { ...user });
+  devtools((set, get) => ({
+    user: SessionStorage.get<IUser>('user') as IUser | null,
+    isAuthenticated: SessionStorage.get<IUser>('user') ? true : null,
+    setUser: (user: IUser) => {
+      SessionStorage.set<IUser>('user', { ...user });
 
-			set({ user, isAuthenticated: true });
-		},
-		setBalance: (balance: number) => {
-			const { user } = get();
-			if (user) {
-				user.balance = balance;
-				set({ user })
-			}
-		},
+      set({ user, isAuthenticated: true });
+    },
+    setBalance: (balance: number) => {
+      const { user } = get();
+      if (user) {
+        user.balance = balance;
+        set({ user });
+      }
+    },
 
-		getUser: () => SessionStorage.get<IUser>('user') as IUser | null,
+    getUser: () => SessionStorage.get<IUser>('user') as IUser | null,
 
-		setIsAuthenticated: (isAuthenticated: boolean | null) =>
-			set({ isAuthenticated }),
-		logOut: () => {
-			set({ user: null, isAuthenticated: false });
-		},
-		isEnrolled: _id => {
-			if (get().user?.role != Role?.TRAINEE) return true; //I will remove the Card Buttons from the Card of the enrolled is true
-			return (
-				[
-					...((get().user as ITrainee)?._enrolledCourses as EnrolledCourse[])
-				].find(item => item?._course?._id === _id) !== undefined
-			);
-		}
-	}))
+    setIsAuthenticated: (isAuthenticated: boolean | null) =>
+      set({ isAuthenticated }),
+    logOut: () => {
+      set({ user: null, isAuthenticated: false });
+    },
+    isEnrolled: _id => {
+      if (get().user?.role != Role?.TRAINEE) return true; //I will remove the Card Buttons from the Card of the enrolled is true
+      return (
+        [
+          ...((get().user as ITrainee)?._enrolledCourses as EnrolledCourse[])
+        ].find(item => item?._course?._id === _id) !== undefined
+      );
+    }
+  }))
 );
 
 export const UseUser = () => useUserStore(state => state.user);
 export const UseUserSetBalance = () => useUserStore(state => state.setBalance);
 
-
 export const UseSetUser = () => useUserStore(state => state.setUser);
 export const UseUserStoreLogOut = () => useUserStore(state => state.logOut);
 export const UseUserIsAuthenticated = () =>
-	useUserStore(state => state.isAuthenticated);
+  useUserStore(state => state.isAuthenticated);
 export const UseUserSetIsAuthenticated = () =>
-	useUserStore(state => state.setIsAuthenticated);
+  useUserStore(state => state.setIsAuthenticated);
