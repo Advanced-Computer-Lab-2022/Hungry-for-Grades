f2de599c4c8253bcc33d5ad3f2d1cb90df6e6c53
Omar Sherif Ali <omar.sherif9992@gmail.com>
2022-12-24 13:15:19 +0200

    fix: bug

--- client/src/store/userStore.ts
+++ client/src/store/userStore.ts
@@ -4,51 +4,63 @@ import { devtools } from 'zustand/middleware';
 
 import SessionStorage from '@/services/sessionStorage/SessionStorage';
 
-import { type IUser } from '@interfaces/user.interface';
-import { EnrolledCourse, ITrainee } from '@/interfaces/course.interface';
 import { Role } from '@/enums/role.enum';
+import { EnrolledCourse, ITrainee } from '@/interfaces/course.interface';
+import { type IUser } from '@interfaces/user.interface';
 
 export interface IUserStore {
-  user: IUser | null;
-  setUser: (user: IUser) => void;
-  getUser: () => IUser | null;
-  logOut: () => void;
-  isAuthenticated: boolean | null;
-  setIsAuthenticated: (isAuthenticated: boolean) => void;
-  isEnrolled: (id: string) => boolean;
+	user: IUser | null;
+	setUser: (user: IUser) => void;
+	getUser: () => IUser | null;
+	logOut: () => void;
+	isAuthenticated: boolean | null;
+	setIsAuthenticated: (isAuthenticated: boolean) => void;
+	isEnrolled: (id: string) => boolean;
+	setBalance: (balance: number) => void
 }
 
 export const useUserStore = create<IUserStore, [['zustand/devtools', never]]>(
-  devtools((set, get) => ({
-    user: SessionStorage.get<IUser>('user') as IUser | null,
-    isAuthenticated: SessionStorage.get<IUser>('user') ? true : null,
-    setUser: (user: IUser) => {
-      SessionStorage.set<IUser>('user', { ...user });
+	devtools((set, get) => ({
+		user: SessionStorage.get<IUser>('user') as IUser | null,
+		isAuthenticated: SessionStorage.get<IUser>('user') ? true : null,
+		setUser: (user: IUser) => {
+			SessionStorage.set<IUser>('user', { ...user });
 
-      set({ user, isAuthenticated: true });
-    },
-    getUser: () => SessionStorage.get<IUser>('user') as IUser | null,
-    setIsAuthenticated: (isAuthenticated: boolean | null) =>
-      set({ isAuthenticated }),
-    logOut: () => {
-      set({ user: null, isAuthenticated: false });
-    },
-    isEnrolled: _id => {
-      if (get().user?.role != Role?.TRAINEE) return true; //I will remove the Card Buttons from the Card of the enrolled is true
-      return (
-        [
-          ...((get().user as ITrainee)?._enrolledCourses as EnrolledCourse[])
-        ].find(item => item?._course?._id === _id) !== undefined
-      );
-    }
-  }))
+			set({ user, isAuthenticated: true });
+		},
+		setBalance: (balance: number) => {
+			const { user } = get();
+			if (user) {
+				user.balance = balance;
+				set({ user })
+			}
+		},
+
+		getUser: () => SessionStorage.get<IUser>('user') as IUser | null,
+
+		setIsAuthenticated: (isAuthenticated: boolean | null) =>
+			set({ isAuthenticated }),
+		logOut: () => {
+			set({ user: null, isAuthenticated: false });
+		},
+		isEnrolled: _id => {
+			if (get().user?.role != Role?.TRAINEE) return true; //I will remove the Card Buttons from the Card of the enrolled is true
+			return (
+				[
+					...((get().user as ITrainee)?._enrolledCourses as EnrolledCourse[])
+				].find(item => item?._course?._id === _id) !== undefined
+			);
+		}
+	}))
 );
 
 export const UseUser = () => useUserStore(state => state.user);
+export const UseUserSetBalance = () => useUserStore(state => state.setBalance);
+
 
 export const UseSetUser = () => useUserStore(state => state.setUser);
 export const UseUserStoreLogOut = () => useUserStore(state => state.logOut);
 export const UseUserIsAuthenticated = () =>
-  useUserStore(state => state.isAuthenticated);
+	useUserStore(state => state.isAuthenticated);
 export const UseUserSetIsAuthenticated = () =>
-  useUserStore(state => state.setIsAuthenticated);
+	useUserStore(state => state.setIsAuthenticated);
