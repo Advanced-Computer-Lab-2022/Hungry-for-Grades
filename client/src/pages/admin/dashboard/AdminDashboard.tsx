75cddf658c9cd4c2a6535c20ef340589ec25006d
Omar Sherif Ali <omar.sherif9992@gmail.com>
2022-12-29 21:37:13 +0200

    feat: icons

--- client/src/pages/admin/dashboard/AdminDashboard.tsx
+++ client/src/pages/admin/dashboard/AdminDashboard.tsx
@@ -1,14 +1,19 @@
-import { RiDashboardFill } from 'react-icons/ri';
+import { RiDashboardFill, RiRefund2Fill } from 'react-icons/ri';
+
+import { VscReport } from 'react-icons/vsc';
+import { AiOutlineUserAdd } from 'react-icons/ai';
+
+import {TbDiscount2} from 'react-icons/tb'
+import {FcInvite} from 'react-icons/fc'
 
 import Dashboard from '@/components/dashboard/Dashboard';
-
 const navLinks = {
   Dashboard: { path: '/admin/dashboard', icon: <RiDashboardFill /> },
-  'Create User': { path: '/admin/create-user', icon: <></> },
-  'Course Requests': { path: '/admin/course-requests', icon: <></> },
-  Reports: { path: '/admin/reports', icon: <></> },
-  Discounts: { path: '/admin/courses-discounts', icon: <></> },
-  Refunds: { path: '/admin/refunds', icon: <></> }
+  'Create User': { path: '/admin/create-user', icon: <AiOutlineUserAdd/> },
+  'Course Requests': { path: '/admin/course-requests', icon:<FcInvite/> },
+  Reports: { path: '/admin/reports', icon: <VscReport/>  },
+  Discounts: { path: '/admin/courses-discounts', icon: <TbDiscount2/> },
+  Refunds: { path: '/admin/refunds', icon: <RiRefund2Fill/> }
   //Profile: { path: '/admin/profile', icon: <FiUser /> }
 };
 
